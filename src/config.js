813ae2d2884d1cacc6f1cd120245ecafb640d46f
ayanshamaoon937 <70017220+ayanshamaoon937@users.noreply.github.com>
2023-06-16 15:07:01 +0500

    changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 let configg = {
-    link: "https://lookbuilderdashboard.shahab01.com/admin/serverside/api.php",
-    img: "https://lookbuilderdashboard.shahab01.com/admin/"
+    // link: "https://lookbuilderdashboard.shahab01.com/admin/serverside/api.php",
+    // img: "https://lookbuilderdashboard.shahab01.com/admin/"
 
-    // link: "http://192.168.1.7/lookbuilder_dashboard/admin/serverside/api.php",
-    // img: "http://192.168.1.7/lookbuilder_dashboard/admin/"
+    link: "https://dashboard.thebestwedding.host/serverside/api.php",
+    img: "https://dashboard.thebestwedding.host/"
 }
+
 export default configg
